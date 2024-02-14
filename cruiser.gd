38063da1a171a7ede0a42bdb78f76b996ee4a60d
Dan Andrus <danieleandrus@gmail.com>
2024-02-14 21:57:17 +0100

    Move cruiser between lanes

--- cruiser.gd
+++ cruiser.gd
@@ -1,14 +1,24 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@export var lanes: Array[Node]
 
+var laneIndex: int
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	laneIndex = (lanes.size() + 1) / 2
+
+func setLane(index: int) -> void:
+	laneIndex = clamp(index, 0, lanes.size() - 1)
+	self.position.x = lanes[laneIndex].position.x
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(_delta: float) -> void:
 	if (Input.is_action_just_pressed("move_left")):
+		setLane(laneIndex - 1)
 		print("Move left")
 	if (Input.is_action_just_pressed("move_right")):
+		setLane(laneIndex + 1)
 		print("Move right")
